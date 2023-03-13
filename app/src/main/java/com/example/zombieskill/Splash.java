f9c962d813cfd40dd17971a150f1fd300f22b1d3
MarioAM96 <91484233+MarioAM96@users.noreply.github.com>
2023-03-13 13:08:08 -0500

    Splash

--- app/src/main/java/com/example/zombieskill/Splash.java
+++ app/src/main/java/com/example/zombieskill/Splash.java
@@ -2,6 +2,7 @@ package com.example.zombieskill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -10,5 +11,17 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        int Duracion_Splash = 1500;
+
+        //Handler EJECUTA UN DETERMINADO CODIGO EN UN TIEMPO DETERMINADO
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //CODIGO QUE EJECUTA EL HANDLER
+                Intent Intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(Intent);
+            };
+        }, Duracion_Splash);
     }
 }

e28407883cfcedf9d7318d6954eb67f9e98a0e61
MarioAM96 <91484233+MarioAM96@users.noreply.github.com>
2023-03-16 21:48:51 -0500

    Login Funcional

--- app/src/main/java/com/example/zombieskill/Login.java
+++ app/src/main/java/com/example/zombieskill/Login.java
@@ -1,15 +1,29 @@
 package com.example.zombieskill;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     EditText    correoLogin, passLogin;
     Button      BtnLogin;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +32,45 @@ public class Login extends AppCompatActivity {
         correoLogin = findViewById(R.id.correoLogin);
         passLogin = findViewById(R.id.passLogin);
         BtnLogin = findViewById(R.id.BtnLogin);
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = correoLogin.getText().toString();
+                String pass = passLogin.getText().toString();
+
+                //Validacion de correo electronico
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    correoLogin.setError("Correno no válido");
+                    correoLogin.setFocusable(true);
+                } else if (pass.length()<6) {
+                    passLogin.setError("La contraseña debe tener almenos 6 caractes");
+                    passLogin.setFocusable(true);
+                }else {
+                    LogeoDeJugador(email,pass);
+                }
+            }
+        });
+    }
+
+    private void LogeoDeJugador(String email, String pass){
+        auth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = auth.getCurrentUser();
+                            startActivity(new Intent(Login.this, Menu.class));
+                            assert user != null; //El usuario no debe ser nulo
+                            Toast.makeText(Login.this, "BIENVENIDO"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, ""+e.getMessage(), Toast.LENGTH_SHORT);
+                    }
+                });
     }
 }
